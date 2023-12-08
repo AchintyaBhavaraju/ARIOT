c7a97d0ffe5a33e1cae1c7a98b6688d3e185a342
AchintyaBhavaraju <Achintyabhavaraju@gmail.com>
2023-12-08 10:17:39 +0530

    Initial Commit

---
+++ IOT/IOTApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOTApp.swift
+//  IOT
+//
+//  Created by Achintya Bhavaraju on 08/12/23.
+//
+
+import SwiftUI
+
+@main
+struct IOTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
